0b8a6d1545f1bb427f7f4ed17b91d6862ed0379f
FurkanArslanx <Profilo12Afurkan@gmail.com>
2024-12-02 11:36:04 +0300

    Add files via upload

---
+++ Stok takip uyg/Amp12A_603_StokTakipProgramı/Amp12A_603_StokTakipProgramı/Model/Kategori.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amp12A_603_StokTakipProgramı.Model
+{
+    public class Kategori
+    {
+        [Key]
+
+        public int Id { get; set; }
+
+        public string KategoriAdi { get; set; }
+
+        // İlişki : Bir Kategorinin birden fazla ürün sahip olabilir
+
+        public virtual ICollection<Urun> Urunler { get; set; } = new HashSet<Urun>();
+    }
+}
